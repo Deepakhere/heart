9a44dcf166562524d58509fc986c6923fe5fb66f
Deepak Gupta <78081310+Deepakhere@users.noreply.github.com>
2021-12-02 00:36:44 +0530

    Add files via upload

---
+++ hrt.py
@@ -0,0 +1,30 @@
+import turtle
+
+s=turtle.Screen().bgcolor('black')
+t = turtle.Turtle()
+t.speed(0)
+t.width(12)
+
+def curve():
+    for i in range(200):
+        t.right(1)
+        t.forward(1)
+
+        def heart():
+            t.color('red','red')
+            t.begin_fill()
+            t.left(140)
+            t.forward(113)
+            curve()
+            t.left(120)
+            curve()
+            t.forward(112)
+            t.end_fill()
+
+            heart()
+            t.penup()
+            t.goto(-260,-20)
+            t.pencolor('red')
+            t.write("I you", align="left", font=("courier", 150, 'bold italic'))
+
+
